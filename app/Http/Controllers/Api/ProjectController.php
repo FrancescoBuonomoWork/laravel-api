a7ff22e32627ea6c16e65f40b548c9027c9e63df
FrancescoBuonomo <buonomofrancescow@gmail.com>
2024-01-15 16:32:50 +0100

    fix type in api project controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -9,7 +9,8 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $results = Project::all();
+        // $results = Project::all();
+        $results = Project::with('type','technologies')->paginate(20);
 
         return  response()->json([
             'success'=> true,
